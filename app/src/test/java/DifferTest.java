6d55a49b1f560b465624241e742fc58f7f78a493
exerusik <exe.rusik@gmail.com>
2023-01-25 13:36:45 +0300

    correct structure

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -9,10 +9,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 class DifferTest {
-    private String filePath1 = "/home/ruslan/java-project-71/app/filepath1.json";
-    private String filePath2 = "/home/ruslan/java-project-71/app/filepath2.json";
+    private String filePath1 = "src/test/resources/filepath1.json";
+    private String filePath2 = "src/test/resources/filepath2.json";
 
-    private Path trueAnswer = Paths.get("/home/ruslan/java-project-71/app/src/test/resources/check");
+    private Path trueAnswer = Paths.get("src/test/resources/check");
 
     @Test
     public void correct() throws Exception {
